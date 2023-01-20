4dcb42c625e056642957c56bf4fe9480cd934363
SandraLeAnn <sharpnw21@gmail.com>
2023-01-20 16:01:51 -0700

    JUnit Test

---
+++ JPATrips/src/main/java/com/skilldistillery/trips/entities/trips.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.trips.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class trips {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	public trips() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "trips [id=" + id + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		trips other = (trips) obj;
+		return id == other.id && Objects.equals(name, other.name);
+	}
+
+}
